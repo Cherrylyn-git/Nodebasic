377853f3773543dfd3a81e88dc970f59a62b37ec
Cherrylyn Panlubasan <panlubasancherrylyn01@gmail.com>
2026-04-28 16:22:32 +0800

    logger update

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@ var attempts = 0;
 var timer = setInterval(function(){
     attempts += 1;
     if (attempts <= 4){
-        console.log(Timer + "Accessing " + __filename);
+        console.log(timer + " Accessing " + __filename);
     }else{
         console.log('Logging Complete. System exiting...');
         clearInterval(timer);
